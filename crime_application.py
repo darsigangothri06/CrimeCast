fbb764bb125fc31e0dcab4ba5aa030e1b3137f58
Gangothri Darsi <77739656+darsigangothri06@users.noreply.github.com>
2024-03-06 22:24:54 +0530

    Update crime_application.py

--- crime_application.py
+++ crime_application.py
@@ -37,49 +37,49 @@ def prediction(x_cor, y_cor, loaded_data):
     return (get_crime_type(predictions.tolist()[0]))
 
 # main
+def main():
+    loaded_data = joblib.load('crimecast_location_based_model.pkl')
+    st.title('Crime Prediction By Location Using Machine Learning')
+    x_cor = st.number_input('Enter Latittude: ', step=0.1)
+    y_cor = st.number_input('Enter Longitude: ', step=0.1)
 
-loaded_data = joblib.load('crimecast_location_based_model.pkl')
-st.title('Crime Prediction By Location Using Machine Learning')
-x_cor = st.number_input('Enter Latittude: ', step=0.1)
-y_cor = st.number_input('Enter Longitude: ', step=0.1)
+    if st.button('Predict crime type'):
+        crime_type = prediction(x_cor, y_cor, loaded_data)
+        if crime_type == 'No Major Crime Happened':
+            st.success(crime_type)
+        else:
+            st.error(crime_type)
 
-if st.button('Predict crime type'):
-    crime_type = prediction(x_cor, y_cor, loaded_data)
-    if crime_type == 'No Major Crime Happened':
-        st.success(crime_type)
-    else:
-        st.error(crime_type)
+    with open('1_number_of_crimes_by_year.json', 'r') as file:
+        graph1 = json.load(file)
 
-with open('1_number_of_crimes_by_year.json', 'r') as file:
-    graph1 = json.load(file)
+    with open('2_count_of_crimes_by_crime_type.json', 'r') as file:
+        graph2 = json.load(file)
 
-with open('2_count_of_crimes_by_crime_type.json', 'r') as file:
-    graph2 = json.load(file)
+    with open('3_top_four_crime_types.json', 'r') as file:
+        graph3 = json.load(file)
 
-with open('3_top_four_crime_types.json', 'r') as file:
-    graph3 = json.load(file)
+    with open('4_count_of_top_four_crimes_by_location.json', 'r') as file:
+        graph4 = json.load(file)
 
-with open('4_count_of_top_four_crimes_by_location.json', 'r') as file:
-    graph4 = json.load(file)
+    with open('5_count_of_all_crimes_by_location.json', 'r') as file:
+        graph5 = json.load(file)
 
-with open('5_count_of_all_crimes_by_location.json', 'r') as file:
-    graph5 = json.load(file)
+    with open('6_number_of_crimes_by_month.json', 'r') as file:
+        graph6 = json.load(file)
 
-with open('6_number_of_crimes_by_month.json', 'r') as file:
-    graph6 = json.load(file)
+    with open('7_count_of_crimes_by_year_and_arrest_status.json', 'r') as file:
+        graph7 = json.load(file)
 
-with open('7_count_of_crimes_by_year_and_arrest_status.json', 'r') as file:
-    graph7 = json.load(file)
+    with open('8_correlation_between_attributes.json', 'r') as file:
+        graph8 = json.load(file)
 
-with open('8_correlation_between_attributes.json', 'r') as file:
-    graph8 = json.load(file)
-
-st.plotly_chart(graph1)
-st.plotly_chart(graph2)
-st.plotly_chart(graph3)
-st.plotly_chart(graph4)
-st.plotly_chart(graph5)
-st.plotly_chart(graph6)
-st.plotly_chart(graph7)
-st.markdown("<span style='font-family: Arial; font-size: 15px;'><b>Correlation between dataset attributes</b></span>", unsafe_allow_html=True)
-st.plotly_chart(graph8)
+    st.plotly_chart(graph1)
+    st.plotly_chart(graph2)
+    st.plotly_chart(graph3)
+    st.plotly_chart(graph4)
+    st.plotly_chart(graph5)
+    st.plotly_chart(graph6)
+    st.plotly_chart(graph7)
+    st.markdown("<span style='font-family: Arial; font-size: 15px;'><b>Correlation between dataset attributes</b></span>", unsafe_allow_html=True)
+    st.plotly_chart(graph8)
